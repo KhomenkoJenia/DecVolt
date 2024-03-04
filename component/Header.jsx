24132fe4bf8dfe364a3849e79cd9faf39d968b31
JeniaKhomenko <jeniaaajeniaaa@gmail.com>
2024-03-04 15:49:39 +0200

    five

--- component/Header.jsx
+++ component/Header.jsx
@@ -49,7 +49,9 @@ function Header() {
               {item.name}
             </a>
           ))}
-          <DarckMode />
+          <div>
+            <DarckMode />
+          </div>
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
